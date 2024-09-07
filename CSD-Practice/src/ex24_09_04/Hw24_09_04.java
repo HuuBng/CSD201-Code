7a20b03097a500b6f2745f4e1a5d3c3341d8b920
HuuBng <89332259+HuuBng@users.noreply.github.com>
2024-09-07 07:30:27 +0700

    Change method name

--- CSD-Practice/src/ex24_09_04/Hw24_09_04.java
+++ CSD-Practice/src/ex24_09_04/Hw24_09_04.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Hw24_09_04 {
 
     // Hw 01: O(n) find second min / max
-    public static int Hw_findSecMin(int[] arr) {
+    public static int findSecondMin(int[] arr) {
         int min = Integer.MAX_VALUE;
         int secMin = Integer.MAX_VALUE;
 
@@ -21,7 +21,7 @@ public class Hw24_09_04 {
         return secMin;
     }
 
-    public static int Hw_findSecMax(int[] arr) {
+    public static int findSecondMax(int[] arr) {
         int max = Integer.MIN_VALUE;
         int secMax = Integer.MIN_VALUE;
 
@@ -38,7 +38,7 @@ public class Hw24_09_04 {
     }
 
     // Hw 02: Check if a String is a Palindrome or not
-    public static void Hw_isPalindrome(String str) {
+    public static void checkPalindrome(String str) {
         String[] str_arr = str.split("");
         String[] result = new String[str_arr.length];
 
@@ -54,7 +54,7 @@ public class Hw24_09_04 {
     }
 
     // Hw 03: Move all zero to the back of an array
-    public static int[] Hw_moveAllZero(int[] arr) {
+    public static int[] moveAllZeroes(int[] arr) {
         int[] result = new int[arr.length];
         int index = 0;
 
@@ -73,18 +73,19 @@ public class Hw24_09_04 {
         System.out.println("\n === Homework 01 ===");
         int[] arr = {5, 3, 2, 8, 9, 3, 5, 77, 43, 22, 68, 12};
         System.out.println("Array: " + Arrays.toString(arr));
-        System.out.println("SecMin: " + Hw_findSecMin(arr));
-        System.out.println("SecMax: " + Hw_findSecMax(arr));
+        System.out.println("SecMin: " + findSecondMin(arr));
+        System.out.println("SecMax: " + findSecondMax(arr));
 
         System.out.println("\n === Homework 02 ===");
-        Hw_isPalindrome("madam");
-        Hw_isPalindrome("test");
-        Hw_isPalindrome("hello");
+        checkPalindrome("madam");
+        checkPalindrome("test");
+        checkPalindrome("hello");
+        checkPalindrome("racecar");
 
         System.out.println("\n === Homework 03 ===");
         int[] arr2 = {8, 0, 5, 0, 2, 0, 0};
         System.out.println("Array: " + Arrays.toString(arr2));
-        System.out.println("MoveAllZero: " + Arrays.toString(Hw_moveAllZero(arr2)));
+        System.out.println("MoveAllZero: " + Arrays.toString(moveAllZeroes(arr2)));
 
     }
 
